46483b05f71e4aa82fe32258ac288c64a64527b3
Taras Paliy <taras505paliy@gmail.com>
2023-12-16 17:21:19 +0200

    remove footer and add nav html

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,19 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 
+import Navigation from "./Navigation/Navigation";
+
 function App() {
   return (
     <>
-      <header id="header">
-        <h3>Taras Palii</h3>
-        <h3>2023</h3>
-      </header>
-      <main id="main">Main</main>
-      <footer id="footer">
-        <h3>Socials</h3>
-        <h3>taras505paliy@gmail.com</h3>
-      </footer>
+      <main id="main">
+        <header id="header">
+          <h1>Taras Palii</h1>
+          <h3>Software Engineer</h3>
+
+          <Navigation />
+        </header>
+      </main>
     </>
   );
 }
---
+++ src/Navigation/Navigation.jsx
@@ -0,0 +1,16 @@
+function Navigation() {
+  return (
+    <nav>
+      <ol>
+        <li>
+          <a>About</a>
+        </li>
+        <li>
+          <a>Contact</a>
+        </li>
+      </ol>
+    </nav>
+  );
+}
+
+export default Navigation;
