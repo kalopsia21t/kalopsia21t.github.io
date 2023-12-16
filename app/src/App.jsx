c048a4c32a63e3bae8d5b435ca5d780ebb31b397
Taras Paliy <taras505paliy@gmail.com>
2023-12-16 16:31:04 +0200

    first commit

---
+++ app/src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <header id="header">
+        <h3>Taras Palii</h3>
+        <h3>2023</h3>
+      </header>
+      <main id="main">Main</main>
+      <footer id="footer">
+        <h3>Socials</h3>
+        <h3>taras505paliy@gmail.com</h3>
+      </footer>
+    </>
+  );
+}
+
+export default App;
